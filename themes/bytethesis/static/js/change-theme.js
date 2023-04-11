2cbd97465b98192de0dc4d883e4be841a1182464
izkeas <izkeasyomito@gmail.com>
2023-04-11 17:08:19 -0300

    Fixed responsivity; search button now works; added change theme on phones

--- themes/bytethesis/static/js/change-theme.js
+++ themes/bytethesis/static/js/change-theme.js
@@ -1,20 +1,19 @@
-const changeTheme = document.getElementById("change-theme-button");
-
 function changeIcon(){
     if (currentTheme == "White"){
-        $("#theme-light-icon").css("display","none")
-        $("#theme-dark-icon").css("display", "block")
+        $(".theme-light-icon").css("display","none")
+        $(".theme-dark-icon").css("display", "block")
     }
     
     else if (currentTheme == "Dark"){
-        $("#theme-light-icon").css("display", "block")
-        $("#theme-dark-icon").css("display", "none")
+        $(".theme-light-icon").css("display", "block")
+        $(".theme-dark-icon").css("display", "none")
     }
 }
 
 changeIcon();
 
-changeTheme.addEventListener("click", (ev) => {
+
+$(".change-theme-button").click( () => {
     toggleTheme();
     changeIcon();
 })
