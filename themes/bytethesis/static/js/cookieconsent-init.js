874af73afdf13e23f8017a902533d82c98b418e2
izkeas <izkeasyomito@gmail.com>
2023-04-13 20:42:31 -0300

    Changed cookie consent

--- themes/bytethesis/static/js/cookieconsent-init.js
+++ themes/bytethesis/static/js/cookieconsent-init.js
@@ -17,7 +17,7 @@ var cc = initCookieConsent();
 
 // run plugin with your configuration
 cc.run({
-    current_lang: 'en',
+    current_lang: 'pt',
     autoclear_cookies: true,                   // default: false
     page_scripts: true,                        // default: false
 
@@ -49,17 +49,24 @@ cc.run({
         // ...
     },
 
+    gui_options : {
+        consent_modal: {
+            layout: 'box',               // box/cloud/bar
+            position: 'middle center',     // bottom/middle/top + left/right/center
+        },
+    },
+
     languages: {
-        'en': {
+        'pt': {
             consent_modal: {
-                title: 'We use cookies!',
-                description: 'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+                title: 'Nós usamos Cookies!',
+                description: 'Este site utiliza cookies para melhorar a sua experiência de navegação e personalizar o conteúdo de acordo com seus interesses. Ao continuar aceitar, você concorda com a nossa política de cookies.',
                 primary_btn: {
-                    text: 'Accept all',
+                    text: 'aceitar todos',
                     role: 'accept_all'              // 'accept_selected' or 'accept_all'
                 },
                 secondary_btn: {
-                    text: 'Reject all',
+                    text: 'rejeitar todos',
                     role: 'accept_necessary'        // 'settings' or 'accept_necessary'
                 }
             },
@@ -75,55 +82,8 @@ cc.run({
                     {col3: 'Expiration'},
                     {col4: 'Description'}
                 ],
-                blocks: [
-                    {
-                        title: 'Cookie usage 📢',
-                        description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.'
-                    }, {
-                        title: 'Strictly necessary cookies',
-                        description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
-                        toggle: {
-                            value: 'necessary',
-                            enabled: true,
-                            readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
-                        }
-                    }, {
-                        title: 'Performance and Analytics cookies',
-                        description: 'These cookies allow the website to remember the choices you have made in the past',
-                        toggle: {
-                            value: 'analytics',     // your cookie category
-                            enabled: false,
-                            readonly: false
-                        },
-                        cookie_table: [             // list of all expected cookies
-                            {
-                                col1: '^_ga',       // match all cookies starting with "_ga"
-                                col2: 'google.com',
-                                col3: '2 years',
-                                col4: 'description ...',
-                                is_regex: true
-                            },
-                            {
-                                col1: '_gid',
-                                col2: 'google.com',
-                                col3: '1 day',
-                                col4: 'description ...',
-                            }
-                        ]
-                    }, {
-                        title: 'Advertisement and Targeting cookies',
-                        description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
-                        toggle: {
-                            value: 'targeting',
-                            enabled: false,
-                            readonly: false
-                        }
-                    }, {
-                        title: 'More information',
-                        description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
-                    }
-                ]
-            }
+                blocks: []
+            },
         }
     }
 });
